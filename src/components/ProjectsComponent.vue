5759093c49ff65d277d895f0fd6b390be71bb049
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-06 16:21:05 +0100

    [ init ] project component with v-for projects

--- src/components/ProjectsComponent.vue
+++ src/components/ProjectsComponent.vue
@@ -1,8 +1,11 @@
 <script>
+import {store} from '../data/store';
   export default {
     name: 'ProjectsComponent',
     data() {
-      return {}
+      return {
+        store
+      }
     },
     computed: {},
     mounted() {},
@@ -13,10 +16,24 @@
 
 
 <template>
-  <div>Projects</div>
+  <h1>Projects</h1>
+  <div class="projects-list" v-for="(project, index) in store.projects" :key="project.id">
+      <h3>{{ index +1 }} {{project.name}}</h3>
+      <p v-html="project.description"></p>
+  </div>
 </template>
 
 
 <style lang="scss" scoped>
 
+h1 {
+  text-align: center;
+  margin: 20px 0;
+}
+.projects-list {
+  padding: 10px;
+  margin: 15px 0;
+  border: 1px solid black;
+  border-radius: 5px;
+}
 </style>
