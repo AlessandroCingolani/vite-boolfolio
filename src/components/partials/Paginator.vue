4fe3f8445502c5c7de5b6680e3ee6a151bc36260
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-06 17:11:01 +0100

    [ add ] props array for take links pages

---
+++ src/components/partials/Paginator.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name:'Paginator',
+  props: {
+    links:Array
+  },
+}
+</script>
+
+
+<template>
+  <div class="nav-btn">
+
+    <button v-for="link in links" :key="link.label" v-html="link.label" :disabled="link.active || !link.url"></button>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+  .nav-btn {
+    margin-top:15px ;
+    text-align: center;
+    button {
+      padding:5px 8px;
+      margin: 3px;
+      &:hover{
+        cursor: pointer;
+      }
+    }
+  }
+
+</style>
