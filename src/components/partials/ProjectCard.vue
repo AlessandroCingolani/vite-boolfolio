e02bcdbaf6fe1fc2868c0cd8a4092fdfbc9e6641
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-06 16:44:15 +0100

    [ init ] project card need style

---
+++ src/components/partials/ProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name:'Cards',
+  props:{
+    name: String,
+    description: String
+  }
+}
+</script>
+
+
+<template>
+    <div class="my_card">
+      <ul>
+        <li>
+          <h3>{{name}}</h3>
+          <p v-html="description"></p>
+        </li>
+      </ul>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+.my_card {
+  ul {
+    list-style:square
+  }
+  margin: 15px 0;
+ 
+  border-radius: 5px;
+  padding: 10px;
+}
+  
+
+
+</style>
