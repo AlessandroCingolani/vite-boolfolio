f42484dcd1f96a758c1fc327d891a42d61245096
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-06 15:45:54 +0100

    [ init ] scaffolding

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script>
+
+import axios from 'axios';
+import { store } from './data/store';
+import ProjectsComponent from './components/ProjectsComponent.vue';
+
+export default {
+  name:'App',
+  components:{
+    ProjectsComponent
+  },
+  data(){
+    return {
+      title: 'Hello Vue!'
+    }
+  },
+  methods: {
+    getApi(){
+      axios.get(store.apiUrl + 'projects')
+      .then(results => {
+          console.log(results.data);
+          store.projects = results.data;
+        })
+      .catch(error => {
+          console.log(error);
+        })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <ProjectsComponent/>
+  </div>
+</template>
+
+
+
+<style lang="scss"> 
+
+</style>
---
+++ src/components/ProjectsComponent.vue
@@ -0,0 +1,22 @@
+<script>
+  export default {
+    name: 'ProjectsComponent',
+    data() {
+      return {}
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+
+
+<template>
+  <div>Projects</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
