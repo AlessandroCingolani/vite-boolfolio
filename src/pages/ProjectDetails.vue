af174fdc03a536e5d853bfde9fafec66800b9b73
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-07 16:06:55 +0100

    [ init ] route project detail with markup

---
+++ src/pages/ProjectDetails.vue
@@ -0,0 +1,30 @@
+  
+  <script>
+  export default {
+    name:'ProjectDetails',
+  }
+  </script>
+
+
+<template>
+  <div class="project-card">
+    <h2>Title project</h2>
+    <p>Type name</p>
+    <p>Tech</p>
+    <img src="" alt="">
+    <p>Start date</p>
+    <p>End date</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .project-card {
+    min-width: 600px;
+    min-height: 600px;
+    padding: 20px;
+    background-color: #d2c8c8;
+    border-radius: 10px;
+    box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  }
+
+</style>
