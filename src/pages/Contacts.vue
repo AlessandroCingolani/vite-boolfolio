9f734aaa7f145cffda8b8fb7d2127e14feacf5ae
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-13 14:42:06 +0100

    [ add ] component contacts form

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,14 +1,27 @@
 <script>
+
+import ContactsForm from '../components/partials/ContactsForm.vue';
+
 export default {
   name:'Contacts',
+  data(){
+    return
+  },
+  components:{
+    ContactsForm
+  } 
 }
 </script>
 
 
 <template>
 <h1>Contacts</h1>
+<ContactsForm/>
 </template>
 
 <style lang="scss">
-
+  h1 {
+    margin-bottom: 30px;
+    text-align: center;
+  }
 </style>
